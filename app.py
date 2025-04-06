cd85210bae1fab9c733ec1c88ea497d8d0b27dbd
TONG <125841821+tong699@users.noreply.github.com>
2025-04-06 21:18:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ run = st.checkbox("▶️ Start Webcam Stream")
 frame_placeholder = st.empty()
 
 if run:
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
 
     if not cap.isOpened():
         st.error("🚨 Could not access the webcam.")
